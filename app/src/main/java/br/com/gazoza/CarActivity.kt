8e91de73178f73732d54cb293d945bf6711e9947
Davi Monteiro <montsgit@gmail.com>
2024-12-07 17:48:05 -0300

    Bug

--- app/src/main/java/br/com/gazoza/CarActivity.kt
+++ app/src/main/java/br/com/gazoza/CarActivity.kt
@@ -1,10 +1,17 @@
 package br.com.gazoza
 
+import android.icu.util.IslamicCalendar.CalculationType
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+const val Key = "RS"
 
 class CarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/br/com/gazoza/PriceActivity.kt
+++ app/src/main/java/br/com/gazoza/PriceActivity.kt
@@ -1,10 +1,14 @@
 package br.com.gazoza
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class PriceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,25 @@ class PriceActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val Price = findViewById<TextInputEditText>(R.id.PriceImput)
+        val Next = findViewById<AppCompatImageButton>(R.id.Next)
+        Next.setOnClickListener {
+            val PriceStr: String = Price.text.toString()
+            if (PriceStr == "") {
+                val snackbar = Snackbar.make(
+                    Price,
+                    "Digite quanto custa o litro do seu combustível.",
+                    Snackbar.LENGTH_LONG
+                )
+                snackbar.show()
+            } else {
+                val RS = PriceStr.toFloat()
+                val GoToNextActivity = Intent(this, Result::class.java)
+                GoToNextActivity.putExtra(Key, RS)
+                startActivity(GoToNextActivity)
+                println(PriceStr)
+            }
         }
     }
 }
